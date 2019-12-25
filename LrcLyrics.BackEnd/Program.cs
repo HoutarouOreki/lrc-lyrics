928fb38a2ca11345a057fd2d2f8ebbf57f6968a3
Hōtarō Oreki <TheZjarek@gmail.com>
2019-12-25 17:25:00 +0100

    Adjust IP addresses

--- LrcLyrics.BackEnd/Program.cs
+++ LrcLyrics.BackEnd/Program.cs
@@ -11,7 +11,7 @@ namespace LrcLyrics.BackEnd
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls("https://localhost:5001", "http://localhost:5000", "https://192.168.0.104:5001", "http://192.168.0.104:5000", "http://*:80", "http://*:81", "https://*:443");
+                    webBuilder.UseStartup<Startup>().UseUrls("https://localhost:5001", "http://localhost:5000", "http://*:80", "http://*:81", "https://*:443");
                 });
     }
 }
